9bfaaf69e07939b1f703d321a639334c35b27ea7
Alfian <arizqi13@uw.edu>
2014-12-14 12:20:27 -0800

    Test

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -6,8 +6,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("Hello World! Just kidding!");
 		System.out.println("Add an extra line");
+		System.out.println("Okay, another line again for practice!");
 	}
 
 }
